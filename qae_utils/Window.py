d7ec338aacfc6811adabc2d6d764f79d398af506
Jacob Cybulski <ironfrown@gmail.com>
2025-09-10 11:10:52 +1000

    Updated ts_add_noise

--- qae_utils/Window.py
+++ qae_utils/Window.py
@@ -35,10 +35,7 @@ def ts_add_noise(vec, noise=0.0, noise_type='uniform',
     if noise == 0:
         return vec
     else:
-        if seed != 0:
-            rng = np.random.default_rng(int(seed))
-        else:
-            rng = np.random.default_rng()
+        rng = np.random.default_rng()
         if noise_type == 'uniform':
             noise_vec = rng.uniform(-1, 1, len(vec))
         elif noise_type == 'normal':
